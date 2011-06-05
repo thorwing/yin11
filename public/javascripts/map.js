4d5e6198a3584aec0f1ac255ea1e480e4b526e90
unknown <Lei@.(none)>
2011-06-05 19:15:56 +0200

    working on profile update 2

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -5,50 +5,53 @@ function add_fields_with_map(link, association, content) {
   var regexp = new RegExp("new_" + association, "g");
   $(link).before(content.replace(regexp, new_id));
 
-  alert("helo");
-  parepare_map();
+  $("#map_container").show();
+  add_listeners();
 }
 
 $(document).ready(function(){
-    parepare_map();
-});
-
-function parepare_map() {
-    map = new BMap.Map("map_container");          // 创建地图实例
+    initialize_map();
 
     var place = $(".map_place").val();
     var street = $(".map_street").val();
     var city = $(".map_city :selected").text();
-    update_map(city, street, place); // 初始化地图
+    update_map(city, street, place);
 
+    add_listeners();
+});
+
+function initialize_map() {
+    map = new BMap.Map("map_container");          // 创建地图实例
+    map.centerAndZoom("北京");    // 初始化地图
     map.enableScrollWheelZoom();  // 开启鼠标滚轮缩放
     map.enableKeyboard();         // 开启键盘控制
     map.enableContinuousZoom();   // 开启连续缩放效果
     map.enableInertialDragging(); // 开启惯性拖拽效果
+}
 
+function add_listeners() {
     $(".map_city").change(function(){
-        var place = $(".map_place").val();
-        var street = $(".map_street").val();
-        var city = $(".map_city :selected").text();
-        update_map(city, street, place);
+        update_map_from_listener(this);
     });
 
     $(".map_street").change(function(){
-        var place = $(".map_place").val();
-        var street = $(".map_street").val();
-        var city = $(".map_city :selected").text();
-        update_map(city, street, place);
+        update_map_from_listener(this);
     });
 
     $(".map_place").change(function(){
-        var place = $(".map_place").val();
-        var street = $(".map_street").val();
-        var city = $(".map_city :selected").text();
-        update_map(city, street, place);
+        update_map_from_listener(this);
     });
 }
 
-function update_map(city, street, place) {
+function update_map_from_listener(control) {
+    var parent = $(control).parents("div.map_address");
+    var place = parent.find(".map_place").val();
+    var street = parent.find(".map_street").val();
+    var city = parent.find(".map_city").find("option:selected").text();
+    update_map(city, street, place, control);
+}
+
+function update_map(city, street, place, control) {
     if (street != null || place != null)
     {
         var myGeo = new BMap.Geocoder(); // 创建地址解析器实例
@@ -58,7 +61,10 @@ function update_map(city, street, place) {
                 map.centerAndZoom(point, 16);
                 map.addOverlay(new BMap.Marker(point));
 
-                $(".map_point").val(point.lng + "," + point.lat);
+                if(control)
+                {
+                    $(control).parents("div.map_address").find(".map_point").val(point.lng + "," + point.lat);
+                }
             }
             else
             {
