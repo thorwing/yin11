623cbdf8eee91f825a9fb2a138464a14fd7bccad
unknown <Lei@.(none)>
2011-08-27 11:00:25 +0800

    fix smtp error

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,9 +3,9 @@ require "development_mail_interceptor.rb"
 ActionMailer::Base.smtp_settings = {
   :address			=> "smtp.gmail.com",
   :port			=> 587,
-  :user_name		=> "staff@yin11.com",
+  :user_name		=> "yin11.food@gmail.com",
   #:domain => "yin11.com",
-  :password		=> "Iam1staff",
+  :password		=> "Youare1hero",
   :authentication		=> "plain",
   :enable_starttls_auto => true
   }
