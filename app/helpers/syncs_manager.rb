aec83d26988576c60b350ea9ad1e6f9e0521e394
unknown <Lei@.(none)>
2011-09-20 16:38:28 +0800

    skip truncating

--- app/helpers/syncs_manager.rb
+++ app/helpers/syncs_manager.rb
@@ -7,7 +7,7 @@ class SyncsManager
     client = OauthChina::Sina.load(:access_token => @user.access_token, :access_token_secret => @user.access_token_secret)
     #client.add_status("同步到新浪微薄..")
     url = "http://api.t.sina.com.cn/statuses/update.xml"
-    message =truncate_content([review.title, review.content].join(": "), 140)
+    message = [review.title, review.content].join(": ")
     response = client.access_token.request(:post, url, :status => message)
     #Logger.new(STDOUT).info response.to_yaml
   end
