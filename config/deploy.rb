69793f9f49c74f7001c4a52e71c7e195634509a5
unknown <Lei@.(none)>
2011-10-03 15:54:28 +0800

    update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,27 +1,21 @@
 load 'deploy/assets'
-
 set :application, "yinkuaizi.com"
-set :repository,  "git@github.com:thorwing/yin11.git"
-
-set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-set :deploy_via, :remote_cache
-
-set :user, "root"
-set :use_sudo, false
-#set :mongodbname_prod, 'yin11_development'
-#set :mongodbname_dev, 'yin11_production'
-
-role :web, "#{application}"                          # Your HTTP server, Apache/etc
-role :app, "#{application}"                          # This may be the same as your `Web` server
-role :db,  "#{application}", :primary => true # This is where Rails migrations will run
+role :web, application                          # Your HTTP server, Apache/etc
+role :app, application}                          # This may be the same as your `Web` server
+role :db,  application, :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
+set :user, "root"
 set :deploy_to, "/var/www/yin11"
+set :deploy_via, :remote_cache
+set :use_sudo, false
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :scm, :git
+set :repository,  "git://github.com/thorwing/yin11.git"
+set :branch, "master"
+
+#set :mongodbname_prod, 'yin11_development'
+#set :mongodbname_dev, 'yin11_production'
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
