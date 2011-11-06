c75f210fef5c23e01bb0bd4771e3fb3fab769340
Kuroro <kuroro@ubuntu.(none)>
2011-11-06 14:45:33 +0800

    by cp 2011.11.6.14:45

--- app/controllers/syncs_controller.rb
+++ app/controllers/syncs_controller.rb
@@ -5,22 +5,37 @@ class SyncsController < ApplicationController
     before_filter(:only => [:follow_yin11, ]) { |c| c.require_permission :normal_user }
 
     def new
-      switch :type
-      when
-        client = OauthChina::Sina.new
+        case params[:type]
+          when "sina"
+             client = OauthChina::Sina.new
+          when "douban"
+             client = OauthChina::Douban.new
+        end
         authorize_url = client.authorize_url
         Rails.cache.write(build_oauth_token_key(client.name, client.oauth_token), client.dump)
         redirect_to authorize_url
-      when
-
     end
 
     def callback
-      client = OauthChina::Sina.load(Rails.cache.read(build_oauth_token_key(params[:type], params[:oauth_token])))
+      site_name_mapping = {"sina" => "Sina", "douban" => "Douban"}
+
+      site_name = site_name_mapping[params[:type]]
+      client = eval("OauthChina::#{site_name}").load(Rails.cache.read(build_oauth_token_key(params[:type], params[:oauth_token])))
       client.authorize(:oauth_verifier => params[:oauth_verifier])
       Logger.new(STDOUT).info client.to_yaml
 
-      user = sync_account(client)
+      case params[:type]
+        when "sina"
+          xml = (client.access_token.get "/account/verify_credentials.xml").body
+        when "douban"
+          #url = "http://api.douban.com/people/%40me"
+          #xml = client.access_token.request(:get, url).body
+
+          xml = (client.access_token.get "http://api.douban.com/people/%40me").body
+      end
+
+      user = sync_account(xml, client)
+
       if user.present?
         flash[:notice] = t("notices.welcome_back", :name => user.screen_name)
       else
@@ -45,23 +60,43 @@ class SyncsController < ApplicationController
       [name, oauth_token].join("_")
     end
 
-    def sync_account(client)
-      results = client.dump
+    def extract_user_info(credentials, results)
 
+       case params[:type]
+        when "sina"
+          user = User.find_or_initialize_by(:provider => params[:type], :uid => credentials["user"]["id"])
+          if user.new_record?
+            user.provider = params[:type]
+            user.uid = credentials["user"]["id"]
+            user.login_name = credentials["user"]["screen_name"]
+            user.access_token = results[:access_token]
+            user.access_token_secret = results[:access_token_secret]
+            user.save!
+          end
+
+        when "douban"
+           user = User.find_or_initialize_by(:provider => params[:type], :uid => credentials["entry"]["db:uid"])
+           if user.new_record?
+            user.provider = params[:type]
+            user.uid = credentials["entry"]["db:uid"]
+            user.login_name = credentials["entry"]["title"]
+            user.access_token = results[:access_token]
+            user.access_token_secret = results[:access_token_secret]
+            user.save!
+           end
+
+       end
+
+
+
+      user
+    end
+
+    def sync_account(xml, client)
+      results =  client.dump
+      credentials = Crack::XML.parse(xml)
       if results[:access_token] && results[:access_token_secret]
-        #access_token = OAuth::AccessToken.new(client.consumer, results[:access_token], results[:access_token_secret])
-        xml = (client.access_token.get "/account/verify_credentials.xml").body
-        credentials = Crack::XML.parse(xml)
-
-        user = User.find_or_initialize_by(:provider => params[:type], :uid => credentials["user"]["id"])
-        if user.new_record?
-          user.provider = params[:type]
-          user.uid = credentials["user"]["id"]
-          user.login_name = credentials["user"]["screen_name"]
-          user.access_token = results[:access_token]
-          user.access_token_secret = results[:access_token_secret]
-          user.save!
-        end
+        user = extract_user_info(credentials, results)
 
         #TODO remember_me?
         if params[:remember_me]
