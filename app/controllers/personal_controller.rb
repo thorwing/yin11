3e5215e61fbe70cfcb9095deb447797c7cc8e9c3
Ray <ray@direwolf.(none)>
2011-11-19 11:27:12 +0800

    update "me" page

--- app/controllers/personal_controller.rb
+++ app/controllers/personal_controller.rb
@@ -1,6 +1,5 @@
 class PersonalController < ApplicationController
   before_filter(:only => [:me]) { |c| c.require_permission :normal_user }
-  layout "two_columns"
 
   def me
     @feeds = current_user.feeds
