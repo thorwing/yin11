8a86daa600253c18ed67a2013027033051192cf3
Kuroro <kuroro@ubuntu.(none)>
2011-11-22 18:00:18 +0800

    by cp 2011.11.22.18:00

--- app/assets/javascripts/personal.js
+++ app/assets/javascripts/personal.js
@@ -1,7 +1,8 @@
 //function auto_load()
 $(function(){
    var the_tabs = $('.tabContainer .tab'); // the current selected tab
-   the_tabs.click(function(e){
+   the_tabs.click(function(e)
+   {
         /* "this" points to the clicked tab hyperlink: */
         var element = $(this);
         if(!element.data('cache'))
@@ -12,14 +13,21 @@ $(function(){
                 $('#contentHolder').html(msg);
                 /* After page was received, add it to the cache for the current hyperlink: */
                 element.data('cache',msg);
+//                $('a[rel*=lightbox]').slimbox();   must be in the callback function, because
+                $('a[rel*=lightbox]').slimbox();
             });
+//            $('a[rel*=lightbox]').slimbox();  this is wrong, because it is outside the call back function, and may be executed  before the call back function
         }
-        else $('#contentHolder').html(element.data('cache'));
+        else
+        {
+            $('#contentHolder').html(element.data('cache'));
+            $('a[rel*=lightbox]').slimbox();
+
+        }
         e.preventDefault();
+   });
 
-     });
-
-     the_tabs.eq(0).click();
+   the_tabs.eq(0).click();
 });
 
 $(function(){     //change the background of the tabs when
@@ -31,3 +39,4 @@ $(function(){     //change the background of the tabs when
           $(this).addClass("selected");
      })
 })
+
