413c6e90f608c6e9bc5c58b64fb544fe3cd636ef
Kuroro <kuroro@ubuntu.(none)>
2011-11-22 15:03:07 +0800

    by cp 2011.11.22.15:03

--- app/controllers/personal_controller.rb
+++ app/controllers/personal_controller.rb
@@ -2,8 +2,6 @@ class PersonalController < ApplicationController
   before_filter(:only => [:me]) { |c| c.require_permission :normal_user }
 
   def me
-    @all_feeds = FeedsManager.pull_feeds(current_user)
-    @my_feeds = current_user.feeds
   end
 
   def my_feeds
