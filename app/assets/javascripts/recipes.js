b7f132881b6957c4706aa90844a3ebf5e4e51735
Kuroro <kuroro@ubuntu.(none)>
2011-12-30 18:24:55 +0800

    by cp 2011.12.30.18:24

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -12,7 +12,27 @@ $(function(){
     });
 });
 
+
+function consistency_check(range)
+{
+//    alert(range);
+    var result = -1;
+    var addedclasses = $(range).find('.addedclass');
+    addedclasses.each(function(index, addedclass){
+        var amount = String($(addedclass).find('.ingredient_amount').val());
+        var name = String($(addedclass).find('.ingredient_name').val());
+
+        if(amount != "" && name == "")
+        {
+//             alert("return is " + (index+1));
+             result = index + 1;
+        }
+    });
+    return result;
+}
+
 $(function(){
+//    check if all the necessory info is given
     $('#recipe_submit').click(function(e){
         var verify_passed = false;
         var has_recipe_name = false;
@@ -20,19 +40,34 @@ $(function(){
         var has_one_step = false;
         var has_one_image =false;
         var has_one_content =false;
+        var warning = false;
         var message = "您忘了添加:\n";
 //   check if all the neccessary info are given
 
 //        1 recipe name should be given
         if(String($('#recipe_name').val()) != ""){
-           has_recipe_name = true;
+            has_recipe_name = true;
         }
         else{
             message += '     - 菜谱名\n';
         }
 
+//          user may forget to give ingredient name but not amount
+        var pos = 0;
+
+        var ranges = new Array(".minor_ingredients", ".major_ingredients");
+        var displays = new Array("主料", "辅料")
+        for (var i = 0; i < ranges.length; i++)
+        {
+            pos = consistency_check(ranges[i]);
+            if(pos !=  -1)
+            {
+                 warning = true;
+                 message += "     -您忘记填写第" + pos + "个"+ displays[i] +"的名称了\n";
+            }
+        }
+
 //        1 at least one ingredient name is given
-//        alert($(".ingredients").find('.ingredient_name').length);
         var name_inputs = $(".ingredients").find('.ingredient_name');
         name_inputs.each(function(index, name_input){
             if(String($(name_input).val()) != ""){
@@ -65,7 +100,7 @@ $(function(){
         if(has_one_content==true || has_one_image == true)
             has_one_step = true;
 
-        if(has_one_step == true && has_one_ingredient == true && has_recipe_name == true)
+        if(has_one_step == true && has_one_ingredient == true && has_recipe_name == true && warning == false)
             verify_passed = true;
 
 
