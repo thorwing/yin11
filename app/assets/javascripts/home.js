3bc92d8672e6fb9b3db53bef4cf635d4ac83781f
griffin <ray@ubuntu.(none)>
2012-01-19 22:56:36 +0800

    add sologan

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,33 +1,33 @@
 //Slider
-function pageLoaded(event, data) {
-    $("#home_slider #silder_tabs .navigator.selected").removeClass('selected');
-    $("#home_slider #silder_tabs #navigator_" + data.page).addClass('selected');
-}
+//function pageLoaded(event, data) {
+//    $("#home_slider #silder_tabs .navigator.selected").removeClass('selected');
+//    $("#home_slider #silder_tabs #navigator_" + data.page).addClass('selected');
+//}
+//
+//jQuery(function($) {
+//      $( "#home_slider #carousel" ).rcarousel({
+//          auto: {enabled: true,
+//          direction:"next",
+//          interval:5000},
+////          start: generatePages,
+//          pageLoaded: pageLoaded,
+//          step: 1,
+//          speed: 700,
+//          visible: 1,
+//          width: 450,
+//          height: 200
+//      });
+//
+//  });
 
-jQuery(function($) {
-      $( "#home_slider #carousel" ).rcarousel({
-          auto: {enabled: true,
-          direction:"next",
-          interval:5000},
-//          start: generatePages,
-          pageLoaded: pageLoaded,
-          step: 1,
-          speed: 700,
-          visible: 1,
-          width: 450,
-          height: 200
-      });
-
-  });
-
-$(function() {
-    $("#home_slider #silder_tabs .navigator").hover(
-        function(){
-            pos = $(this).data('page');
-            $("#home_slider #silder_tabs .navigator.selected").removeClass('selected');
-            $(this).addClass('selected');
-
-            $( "#home_header #carousel").rcarousel( "goToPage", parseInt(pos) );
-        },
-        function(){});
-});
+//$(function() {
+//    $("#home_slider #silder_tabs .navigator").hover(
+//        function(){
+//            pos = $(this).data('page');
+//            $("#home_slider #silder_tabs .navigator.selected").removeClass('selected');
+//            $(this).addClass('selected');
+//
+//            $( "#home_header #carousel").rcarousel( "goToPage", parseInt(pos) );
+//        },
+//        function(){});
+//});
