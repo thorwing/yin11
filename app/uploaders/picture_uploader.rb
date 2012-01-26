4014cebdc55f7716f1e3761272a505f3fa0d388f
griffin <ray@ubuntu.(none)>
2012-01-26 14:57:59 +0800

    improve process taobao product

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -57,7 +57,8 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
-     %w(jpg jpeg gif png)
+    ['jpg', 'jpeg', 'gif', 'png', '']
+     #%w(jpg jpeg gif png)
   end
 
   # Override the filename of the uploaded files:
