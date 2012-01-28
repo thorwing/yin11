7c323892d46c4525abdbf51fdde0e6cfe07b8fd7
griffin <ray@ubuntu.(none)>
2012-01-28 15:08:37 +0800

    fetch newest desires instead of recommendation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,27 +6,8 @@ class HomeController < ApplicationController
     @recommended_albums = Album.recommended.desc(:priority).limit(RECOMENDED_ALBUMS_ON_HOME_PAGE)
     @stars = User.enabled.masters.sort_by{|master| -1 * master.score}[0..7]
 
-    #the default way:
-    #configured_tags = get_desired_tags_config
-    #@desired_tags = Tag.any_in(name: configured_tags).sort_by {|tag| configured_tags.index(tag.name)}
-    #
-    #more_desires = Desire.recommended.tagged_with(@desired_tags.map(&:name)).desc(:priority)
-    #result = {}
-    #@desired_tags.each do |tag|
-    #  result[tag.name] = more_desires.select{|d| d.tags.include?(tag.name)}.take(10)
-    #end
-    #@desired_tags.reject!{|t| result[t.name].empty?}
-    #@desires = result.inject([]){|memo, (key, values)| memo | values}.compact.uniq
-
-    @desired_tags = Tag.desc(:desires_count).limit(7)
-
-    more_desires = Desire.recommended.tagged_with(@desired_tags.map(&:name)).desc(:priority)
-    result = {}
-    @desired_tags.each do |tag|
-      result[tag.name] = more_desires.select{|d| d.tags.include?(tag.name) && d.get_image_url.present?}.take(10)
-    end
-    @desired_tags = @desired_tags.reject{|t| result[t.name].empty?}
-    @desires = result.inject([]){|memo, (key, values)| memo | values}.compact.uniq
+    @desired_tags = get_desired_tags
+    @desires = @desired_tags.inject([]){|memo, (k, v)| memo | v }.compact.uniq
 
     @primary_tags = get_primary_tags
   end
@@ -49,5 +30,39 @@ class HomeController < ApplicationController
 
   def collect_intro
 
+  end
+
+  private
+
+  def get_desired_tags
+    #the default way:
+    #configured_tags = get_desired_tags_config
+    #@desired_tags = Tag.any_in(name: configured_tags).sort_by {|tag| configured_tags.index(tag.name)}
+    #
+    #more_desires = Desire.recommended.tagged_with(@desired_tags.map(&:name)).desc(:priority)
+    #result = {}
+    #@desired_tags.each do |tag|
+    #  result[tag.name] = more_desires.select{|d| d.tags.include?(tag.name)}.take(10)
+    #end
+    #@desired_tags.reject!{|t| result[t.name].empty?}
+    #@desires = result.inject([]){|memo, (key, values)| memo | values}.compact.uniq
+    desired_tags = Rails.cache.fetch('desired_tags')
+    if desired_tags.nil?
+      desired_tags = {}
+      desires = Desire.where(:priority.gte => 0).excludes(tags: []).desc(:created_at)
+      desires.each do |desire|
+        desire.tags.each do |tag|
+          desired_tags[tag] ||= []
+          desired_tags[tag] |= [desire]
+        end
+      end
+
+      top_tags = desired_tags.inject({}){|memo, (k, v)| memo[k] = v.size; memo}.sort_by{|k, v| -1 * v}.map{|i| i[0]}.take(7)
+      desired_tags = top_tags.inject({}){|memo, tag| memo[tag] = desired_tags[tag]; memo}
+      desired_tags.each {|k,v| desired_tags[k].sort!{|a, b| b.created_at <=> a.created_at}.take(10)}
+      Rails.cache.write('desired_tags', desired_tags, :expires_in => 2.hours) if Rails.env.production?
+    end
+
+    return desired_tags
   end
 end
