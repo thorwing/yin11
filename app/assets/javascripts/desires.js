5bd6c03218faa1752267da59166823fa79dd6a8b
griffin <ray@ubuntu.(none)>
2012-02-01 21:04:58 +0800

    fix some bugs

--- app/assets/javascripts/desires.js
+++ app/assets/javascripts/desires.js
@@ -2,16 +2,16 @@
 function shift_masonry(filter) {
     $('#filters .filter_tab.selected').removeClass('selected');
     $(filter).parents(".filter_tab").addClass('selected');
-    $('#filters .filter_tab .small_bar').width(0);
-
-    var small_bar = $(filter).parents(".filter_tab").find(".small_bar");
-    small_bar.animate ({
-        width: '100%'
-    }, 500, function(){
-    // TODO  make sure if a new tab selected during the duration, the old one is cleared
-        $('#filters .filter_tab .small_bar').width(0);
-        $('#filters .filter_tab.selected .small_bar').width('100%');
-    });
+//    $('#filters .filter_tab .small_bar').width(0);
+//
+//    var small_bar = $(filter).parents(".filter_tab").find(".small_bar");
+//    small_bar.animate ({
+//        width: '100%'
+//    }, 500, function(){
+//    // TODO  make sure if a new tab selected during the duration, the old one is cleared
+//        $('#filters .filter_tab .small_bar').width(0);
+//        $('#filters .filter_tab.selected .small_bar').width('100%');
+//    });
 
     var $container = $('#masonry_container');
     var selector = $(filter).attr('data-filter');
