0e594ff9859ff924eee1340c02ff779f6fd8ab5f
thorwing <wing831223@gmail.com>
2012-02-24 12:14:28 +0800

    working on Solution & Vote

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,14 +16,11 @@ class HomeController < ApplicationController
       @current_mode = "newest"
     end
 
-    @desires = get_desires(@current_mode, 1)
+    @desires, @total_chapters = get_desires( @current_mode, params[:page], params[:chapter])
   end
 
   def more_desires
-    if params[:page].to_i <= PAGES_PER_CHAPTER
-      @desires = get_desires(params[:mode], params[:page])
-    end
-    @desires ||= []
+    @desires, dummy = get_desires(params[:mode], params[:page])
 
     respond_to do |format|
       format.html {render :more_desires, :layout => false}
@@ -45,15 +42,30 @@ class HomeController < ApplicationController
 
   private
 
-  def get_desires(mode, page = 1)
-    desires = []
-    criteria = Desire.all
-    if mode == "hottest"
-      desires = criteria.desc(:admirer_ids).page(page).per(ITEMS_PER_PAGE_FEW)
-    elsif mode == "newest"
-      desires = criteria.desc(:created_at).page(page).per(ITEMS_PER_PAGE_FEW)
+  def get_desires(mode, page = 1, chapter = nil)
+    total_chapters = 0
+    if chapter.present?
+      chapter = chapter.to_i
+    elsif session[:current_desires_chapter].present?
+      chapter = session[:current_desires_chapter].to_i
+    else
+      chapter = 1
     end
 
-    desires
+    page = (page ? page.to_i : 1)
+    criteria = Desire.all
+    desires = []
+    total_chapters = (criteria.size.to_f / PAGES_PER_CHAPTER.to_f / ITEMS_PER_PAGE_FEW.to_f).ceil
+    if page <= PAGES_PER_CHAPTER
+      if mode == "hottest"
+        desires = criteria.desc(:admirer_ids).page((chapter - 1) * PAGES_PER_CHAPTER + page).per(ITEMS_PER_PAGE_FEW)
+      elsif mode == "newest"
+        desires = criteria.desc(:created_at).page((chapter - 1) * PAGES_PER_CHAPTER + page).per(ITEMS_PER_PAGE_FEW)
+      end
+
+      return desires, total_chapters
+    else
+      return [], total_chapters
+    end
   end
 end
