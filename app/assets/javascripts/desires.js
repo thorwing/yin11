bb22dd8803775ecae9262b0a77d2d69eaf5cdd29
thorwing <wing831223@gmail.com>
2012-03-25 22:49:57 +0800

    fix several bugs

--- app/assets/javascripts/desires.js
+++ app/assets/javascripts/desires.js
@@ -189,3 +189,20 @@ $(function() {
         }
     );
 });
+
+function link_product()
+{
+    var count = $('#images_container .product').size();
+    if (count >= products_limit) {
+        alert("最多只能添加3件商品");
+        return;
+    }
+
+    jQuery.facebox($('#product_linker').html());
+};
+
+function pre_link_product(link)
+{
+    $(link).nextAll("#link_spinner").show();
+    return true;
+};
